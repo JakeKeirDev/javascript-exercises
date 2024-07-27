c228559f448edd3897f286571524a7fff4d4fb45
Jake Keir <jakekeirdev@gmail.com>
2024-07-27 13:48:22 +1000

    Completed exercises 3 and 4.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let stringArray = str.split('');
+    let reversedString = '';
+    for (i = stringArray.length -1; i >= 0; i--) {
+        reversedString += stringArray[i];
+    }
+    return(reversedString);
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(numArray, removedNum, ...others) {
+
+    for (let i = numArray.length - 1; i >= 0; i--) {
+        if (numArray[i] === removedNum || others.includes(numArray[i])) {
+            numArray.splice(i, 1);
+        }
+    }
+    return (numArray);
 };
 
 // Do not edit below this line
